142ee957182f44655d566c568dfe2f1510b8bbe0
fruitspunchsamurai <akhiloar@users.noreply.github.com>
2017-04-29 17:30:38 -0500

    Added files for player and game_engine

---
+++ poker_cards/game_engine.py
@@ -0,0 +1,2 @@
+#! /usr/bin/env/python
+"""Game engine that runs initializes player, deck and handles turns"""
---
+++ poker_cards/player.py
@@ -0,0 +1,2 @@
+#! /usr/bin/env/python
+"""Player class used to keep track of players, their cards, and money"""
