1745ba5683b5c3131fe266ad551f39bd1b8e6717
fruitspunchsamurai <akhiloar@users.noreply.github.com>
2017-05-13 17:44:10 -0500

    Added stack and deque data structures to keep track of betting and turns Implemented by using deques for turn rotation and call/check/raise completions and stack for players that have folded.

--- poker_cards/game_engine.py
+++ poker_cards/game_engine.py
@@ -2,10 +2,11 @@
 """Game engine that runs initializes player, deck and handles turns"""
 
 from poker_cards import cards, player
+from collections import deque
 
 
 class Game(object):
-    def __init__(self, num_players=3, deck=cards.Deck(), start_money=50000):
+    def __init__(self, num_players=4, deck=cards.Deck(), start_money=50000):
         self.rounds = 0
         self.pot = 0
         self.ante = 0
@@ -14,16 +15,24 @@ class Game(object):
         self.winner = None
         self.hand_won = None
         self.bet = None
+        self.folded = None
+        self.turn_queue = None
+        self.call_check = None
+        self.dealer = None
+        self.small_blind = None
+        self.big_blind = None
+        self.position = 0
 
     def individual_turn(self, competitor):
         # Individual player chooses to fold, check, call, or raise
         choice = None
         if not self.bet:
+
             while choice.lower() not in ['check', 'fold', 'raise']:
                 choice = input('Check, Fold, or Raise?')
 
             if choice.lower() == 'check':
-                competitor.bet_status = 1
+                self.call_check.append(self.turn_queue.popleft())
 
             elif choice.lower() == 'raise':
                 competitor.bet_value = input('Choose a number between {} and {}'.format(str(self.ante),
@@ -33,10 +42,16 @@ class Game(object):
                                                                                             str(competitor.money)))
                 if int(competitor.bet_value) == competitor.money:
                     print('Going all in!!')
+
                 self.bet = competitor.bet_value
 
+                while len(self.call_check) > 0:
+                    self.turn_queue.append(self.call_check.popleft())
+
+                self.call_check.append(self.turn_queue.popleft())
+
             elif choice.lower() == 'fold':
-                competitor.bet_status = 0
+                self.folded.append(self.turn_queue.popleft())
 
         elif self.bet:
 
@@ -45,6 +60,7 @@ class Game(object):
 
             if choice.lower() == 'call':
                 competitor.bet_value = self.bet
+                self.call_check.append(self.turn_queue.popleft())
 
             elif choice.lower() == 'raise':
                 competitor.bet_value = input(
@@ -56,36 +72,48 @@ class Game(object):
                                                                    str(competitor.money)))
                 if int(competitor.bet_value) == competitor.money:
                     print('Going all in!!')
+
                 self.bet = competitor.bet_value
+
+                while len(self.call_check) > 0:
+                    self.turn_queue.append(self.call_check.popleft())
+
+                self.call_check.append(self.turn_queue.popleft())
+
             elif choice.lower() == 'fold':
-                competitor.bet_status = 0
-            pass
-        return choice.lower(), competitor.bet_value
+                self.folded.append(self.turn_queue.popleft())
+
+                # return choice.lower(), competitor.bet_value
 
     def showdown(self):
         # call hand evaluator here to figure out winner
         pass
 
     def turn(self):
+
         if self.rounds == 0:
-            for competitor in self.players:
-                if competitor.bet_status != 0:
-                    self.individual_turn(competitor)
+            for competitor in self.turn_queue:
+                self.individual_turn(competitor)
         if self.rounds == 1:
-            for competitor in self.players:
-                if competitor.bet_status != 0:
-                    self.individual_turn(competitor)
+            for competitor in self.turn_queue:
+                self.individual_turn(competitor)
         if self.rounds == 2:
-            for competitor in self.players:
-                if competitor.bet_status != 0:
-                    self.individual_turn(competitor)
+            for competitor in self.turn_queue:
+                self.individual_turn(competitor)
         if self.rounds == 3:
-            for competitor in self.players:
-                if competitor.bet_status != 0:
-                    self.individual_turn(competitor)
+            for competitor in self.turn_queue:
+                self.individual_turn(competitor)
         self.rounds += 1
 
     def round(self):
+
+        self.turn_queue = deque(self.players)
+        self.call_check = deque()
+        self.folded = []
+        self.dealer = self.turn_queue[self.position]
+        self.small_blind = self.turn_queue[self.position + 1]
+        self.small_blind = self.turn_queue[self.position + 2]
+
         for competitor in self.players:
             self.deck.move_cards(competitor.hand, 2)
         # Add queue to help betting flow here. Use players from self.players
@@ -93,6 +121,8 @@ class Game(object):
             self.turn()
         if self.rounds > 3 and not self.hand_won:
             self.showdown()
+
+        self.position += 1
 
     def redistribute(self):
         # Redistributes money based on who won the hand
