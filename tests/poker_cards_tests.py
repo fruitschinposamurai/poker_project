8079f73184b047e21e76753366f43cb32dc13e73
fruitspunchsamurai <akhiloar@users.noreply.github.com>
2017-05-17 23:01:52 -0500

    Added stubs for testing in test file

---
+++ tests/poker_cards_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import poker_cards
+
+class TestCards(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+    @classmethod
+    def teardown_class(cls):
+        pass
